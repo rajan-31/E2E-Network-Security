5906e07eb9f29ff788498ef45095a8f06ff9252b
GANGASAGAR HL <gangasagarhl1112@gmail.com>
2025-12-13 18:41:34 +0530

    feat: multi-stage builds

--- backend/src/NetworkSecurity/components/model_train.py
+++ backend/src/NetworkSecurity/components/model_train.py
@@ -6,7 +6,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import GradientBoostingClassifier, AdaBoostClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
-from xgboost import XGBClassifier
+# from xgboost import XGBClassifier
 import mlflow
 from sklearn.preprocessing import StandardScaler
 import pickle
@@ -14,7 +14,6 @@ from urllib.parse import urlparse
 from mlflow.models import infer_signature
 from sklearn.model_selection import GridSearchCV
 from dotenv import load_dotenv
-import datetime
 import os
 from src.NetworkSecurity.entity.config_entity import ModelTrainerConfig
 
@@ -43,8 +42,7 @@ class ModelTrainer:
             "GradientBoostingClassifier": GradientBoostingClassifier(),
             "AdaBoostClassifier": AdaBoostClassifier(),
             "KNeighborsClassifier": KNeighborsClassifier(),
-            "SVC": SVC(),
-            "XGBClassifier": XGBClassifier(),
+            "SVC": SVC()
         }
 
         # Standardize features
